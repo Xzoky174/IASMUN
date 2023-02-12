333be633de15ced2d5611ebebae14f6467221c34
Xzoky174 <xzoky123@gmail.com>
2023-02-12 14:42:52 +0400

    Added Loading Text when Countdown is Loading

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -71,7 +71,8 @@
 <div class="hero">
 	<Saos once={true} animation="fade-in 0.8s cubic-bezier(0.390, 0.575, 0.565, 1.000) both">
 		<p class="countdown">
-			<span class="header">Next Conference:</span>{countDownDate}<br />(10th March 2023)
+			<span class="header">Next Conference:</span>{countDownDate || 'Loading...'}<br />(10th March
+			2023)
 		</p>
 	</Saos>
 
