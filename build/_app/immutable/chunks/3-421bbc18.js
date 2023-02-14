eb3949876a87f14452ae38aac6a2eb9635280263
Xzoky174 <xzoky123@gmail.com>
2023-02-14 23:45:00 +0400

    Added Deploy Command

---
+++ build/_app/immutable/chunks/3-421bbc18.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/coming-soon/_page.svelte-5caa2faa.js";export{t as component};
---
+++ build/_app/immutable/chunks/4-bd427c7b.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/committees/_page.svelte-de6b1bcb.js";export{t as component};
