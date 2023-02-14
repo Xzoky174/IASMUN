68d2c092ae20ff9ddc11406ab2407785df6180d2
Xzoky174 <xzoky123@gmail.com>
2023-02-15 00:17:58 +0400

    Updates

---
+++ _app/immutable/chunks/5-bc92d7d8.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/conference-schedule/_page.svelte-46597ad5.js";export{t as component};
---
+++ _app/immutable/chunks/Vector 3-5c341fca.js
@@ -0,0 +1,1 @@
+const e=""+new URL("../assets/Vector 3-ff6ef2b0.svg",import.meta.url).href;export{e as L};
