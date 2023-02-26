4ec0387f1de1c7cea70593b95171294ed33e50ca
Xzoky174 <xzoky123@gmail.com>
2023-02-26 16:03:46 +0400

    Updates

---
+++ _app/immutable/chunks/1-6faca0e5.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_error.svelte-2fb48c19.js";export{t as component};
---
+++ _app/immutable/chunks/4-f975dd9a.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/committees/_page.svelte-827e7028.js";export{t as component};
