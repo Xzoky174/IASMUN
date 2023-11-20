189e4dfac844a8af083b4915e9ab144116dd51a7
Xzoky174 <xzoky123@gmail.com>
2023-11-20 15:02:33 +0400

    Updates

---
+++ _app/immutable/nodes/4.01x0H_fm.js
@@ -0,0 +1,76 @@
+import{s as ve,r as ge,f as c,a as v,L as $e,g as u,d as l,c as $,h as p,j as n,u as d,i as m,v as be,o as _e,B as se,A as b,y as K}from"../chunks/scheduler.fHUatWhO.js";import{S as xe,i as we,b as x,d as w,m as y,a as g,t as I,e as M,f as ye}from"../chunks/index.-ldY5RpZ.js";import{g as Ie}from"../chunks/globals.0cDDIVm6.js";import{I as Me}from"../chunks/IASMUN.nJrdzFTi.js";import{f as Se}from"../chunks/index.ZRYJZhYD.js";import{S}from"../chunks/Saos.RVjxuPlZ.js";const Ae=""+new URL("../assets/School.KhyieawG.webp",import.meta.url).href,De=""+new URL("../assets/Aya Bawaba.8BHECgaJ.webp",import.meta.url).href,Ue=""+new URL("../assets/Yousef Naser.Iweb6zPo.webp",import.meta.url).href,Ne=""+new URL("../assets/Rushda Saeed.M0wRLPgd.webp",import.meta.url).href,{window:pe}=Ie;function me(i){let e,a,t;return{c(){e=c("img"),this.h()},l(o){e=u(o,"IMG",{class:!0,src:!0,alt:!0}),this.h()},h(){n(e,"class","IASMUN-pic svelte-xo0eug"),se(e.src,a=Me)||n(e,"src",a),n(e,"alt","IASMUN")},m(o,f){m(o,e,f)},i(o){o&&(t||ge(()=>{t=ye(e,Se,{y:300,duration:1800}),t.start()}))},o:b,d(o){o&&l(e)}}}function Ge(i){let e,a='Secretary General&#39;s<br class="svelte-xo0eug"/><span class="welcome svelte-xo0eug">Welcome</span>';return{c(){e=c("h1"),e.innerHTML=a,this.h()},l(t){e=u(t,"H1",{class:!0,"data-svelte-h":!0}),K(e)!=="svelte-jrvr3x"&&(e.innerHTML=a),this.h()},h(){n(e,"class","svelte-xo0eug")},m(t,o){m(t,e,o)},p:b,d(t){t&&l(e)}}}function ke(i){let e,a=`Dear Esteemed Delegates, Advisors, and Distinguished Guests,
+						<br class="svelte-xo0eug"/><br class="svelte-xo0eug"/>
+						It is with immense pleasure and anticipation that I extend a warm welcome to you all to the
+						International Academic School Model United Nations 2024! As the Secretary-General of IASMUN,
+						I am honored to be part of this prestigious event, and I look forward to an enriching and
+						engaging experience for each and every participant.
+						<br class="svelte-xo0eug"/><br class="svelte-xo0eug"/>
+
+						This year&#39;s conference is not just about simulation; it&#39;s about fostering collaboration,
+						embracing diversity, and cultivating the spirit of diplomacy. As delegates, you play a
+						crucial role in this endeavor. Your passion, intellect, and commitment to diplomacy are
+						the driving forces behind the success of IASMUN.
+						<br class="svelte-xo0eug"/><br class="svelte-xo0eug"/>
+
+						Our dedicated team has worked tirelessly to ensure that this conference surpasses
+						expectations. From thought-provoking committees to expertly crafted agendas, we aim to
+						provide an environment that encourages intellectual growth, cultural exchange, and
+						lasting friendships.
+						<br class="svelte-xo0eug"/><br class="svelte-xo0eug"/>
+
+						Whether you are a seasoned delegate or a first-time participant, IASMUN 2024 is your
+						stage to shine. Seize the opportunity to engage in constructive dialogue, showcase your
+						diplomatic skills, and contribute to the resolutions that will shape the future of our
+						global community.
+						<br class="svelte-xo0eug"/><br class="svelte-xo0eug"/>
+
+						I extend my best wishes to each of you as you embark on this MUN journey. May your
+						experiences be enlightening, your debates impactful, and your connections enduring.
+						<br class="svelte-xo0eug"/><br class="svelte-xo0eug"/>
+
+						Looking forward to meeting you all and witnessing the remarkable contributions you will
+						undoubtedly bring to IASMUN 2024!
+						<br class="svelte-xo0eug"/><br class="svelte-xo0eug"/>
+
+						Warmest Regards,
+						<br class="svelte-xo0eug"/>
+						Aya Bawaba
+						<br class="svelte-xo0eug"/>
+						IASMUN&#39;24 Secretary-General`;return{c(){e=c("p"),e.innerHTML=a,this.h()},l(t){e=u(t,"P",{class:!0,"data-svelte-h":!0}),K(e)!=="svelte-13y1rwu"&&(e.innerHTML=a),this.h()},h(){n(e,"class","svelte-xo0eug")},m(t,o){m(t,e,o)},p:b,d(t){t&&l(e)}}}function Le(i){let e,a;return{c(){e=c("img"),this.h()},l(t){e=u(t,"IMG",{src:!0,alt:!0,class:!0}),this.h()},h(){se(e.src,a=De)||n(e,"src",a),n(e,"alt","Secretary General"),n(e,"class","svelte-xo0eug")},m(t,o){m(t,e,o)},p:b,d(t){t&&l(e)}}}function Ee(i){let e,a='Deputy Secretary General&#39;s<br class="svelte-xo0eug"/><span class="welcome svelte-xo0eug">Welcome</span>';return{c(){e=c("h1"),e.innerHTML=a,this.h()},l(t){e=u(t,"H1",{class:!0,"data-svelte-h":!0}),K(e)!=="svelte-1fchuqu"&&(e.innerHTML=a),this.h()},h(){n(e,"class","svelte-xo0eug")},m(t,o){m(t,e,o)},p:b,d(t){t&&l(e)}}}function He(i){let e,a;return{c(){e=c("img"),this.h()},l(t){e=u(t,"IMG",{src:!0,alt:!0,class:!0}),this.h()},h(){se(e.src,a=Ue)||n(e,"src",a),n(e,"alt","Deputy Secretary General"),n(e,"class","svelte-xo0eug")},m(t,o){m(t,e,o)},p:b,d(t){t&&l(e)}}}function ze(i){let e,a=`Hello there, I am Yousef Naser, the Deputy Secretary-General. I am honored to have you
+						join or learn about us. As the Deputy Secretary-General, I have a role to ensure proper
+						procedures are taken and to support my fellow organizers. Whenever or wherever a problem
+						might arise, I will be there to help. Each member, delegate, and advisor must be thanked
+						and respected; each one of them has worked hard to reach this point, to learn and teach.
+						IASMUN has the most ambitious people working to ensure it successfully fulfills its
+						goals. I am proud to be part of IASMUN, and I will be there to support you if any
+						problems occur.`;return{c(){e=c("p"),e.textContent=a,this.h()},l(t){e=u(t,"P",{class:!0,"data-svelte-h":!0}),K(e)!=="svelte-1thcphy"&&(e.textContent=a),this.h()},h(){n(e,"class","svelte-xo0eug")},m(t,o){m(t,e,o)},p:b,d(t){t&&l(e)}}}function Ce(i){let e,a='Chief Advisor&#39;s<br class="svelte-xo0eug"/><span class="welcome svelte-xo0eug">Welcome</span>';return{c(){e=c("h1"),e.innerHTML=a,this.h()},l(t){e=u(t,"H1",{class:!0,"data-svelte-h":!0}),K(e)!=="svelte-r6x09c"&&(e.innerHTML=a),this.h()},h(){n(e,"class","svelte-xo0eug")},m(t,o){m(t,e,o)},p:b,d(t){t&&l(e)}}}function Te(i){let e,a=`Dear Respected Advisors, Guests, and Delegates,
+						<br class="svelte-xo0eug"/><br class="svelte-xo0eug"/>
+						As the Chief Advisor of the IAS Model United Nations (MUN) conference, it gives me great
+						pleasure to greet each of you with a warm embrace. I am thrilled to see the combined intelligence
+						and enthusiasm that each delegate brings to the table as we set out on this exciting journey
+						of diplomacy, dialogue, and collaboration.
+						<br class="svelte-xo0eug"/><br class="svelte-xo0eug"/>
+
+						Aspiring leaders have a rare opportunity to participate in productive discussions, work
+						out solutions, and create cross-border relationships through the Model UN. This
+						simulation helps them improve their diplomatic abilities while also enhancing their
+						comprehension of world affairs and the intricacies of international relations.
+						<br class="svelte-xo0eug"/><br class="svelte-xo0eug"/>
+
+						I urge you to come to this conference with an open mind, a willingness to work together,
+						and a determination to discover points of agreement. Recall that each of you has a
+						significant influence on how our simulations turn out, and your participation is
+						essential to the event&#39;s success.
+						<br class="svelte-xo0eug"/><br class="svelte-xo0eug"/>
+
+						Greetings once more from the Model UN conference. I hope you have a rewarding,
+						enlightening, and enriching experience.
+						<br class="svelte-xo0eug"/><br class="svelte-xo0eug"/>
+
+						Sincerely,
+						<br class="svelte-xo0eug"/>
+
+						Rushda Saeed
+						<br class="svelte-xo0eug"/>
+
+						(Chief Advisor IAS-MUN)`;return{c(){e=c("p"),e.innerHTML=a,this.h()},l(t){e=u(t,"P",{class:!0,"data-svelte-h":!0}),K(e)!=="svelte-t1yvqi"&&(e.innerHTML=a),this.h()},h(){n(e,"class","svelte-xo0eug")},m(t,o){m(t,e,o)},p:b,d(t){t&&l(e)}}}function Ve(i){let e,a;return{c(){e=c("img"),this.h()},l(t){e=u(t,"IMG",{src:!0,alt:!0,class:!0}),this.h()},h(){se(e.src,a=Ne)||n(e,"src",a),n(e,"alt","Chief Advisor"),n(e,"class","svelte-xo0eug")},m(t,o){m(t,e,o)},p:b,d(t){t&&l(e)}}}function Re(i){let e,a,t,o,f,A,D,ae,U,O,N,oe,G,ne,k,L,re,E,H,le,F,z,ie,C,T,ce,V,J,R,ue,W,ee,he,de;ge(i[2]);let h=i[0]&&me();return D=new S({props:{once:!0,animation:"slide-bottom 1.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;",$$slots:{default:[Ge]},$$scope:{ctx:i}}}),N=new S({props:{once:!0,animation:"fade-in 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000) both",$$slots:{default:[ke]},$$scope:{ctx:i}}}),G=new S({props:{once:!0,animation:"slide-left 1.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;",$$slots:{default:[Le]},$$scope:{ctx:i}}}),L=new S({props:{once:!0,animation:"slide-bottom 1.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;",$$slots:{default:[Ee]},$$scope:{ctx:i}}}),H=new S({props:{once:!0,animation:"slide-left 1.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;",$$slots:{default:[He]},$$scope:{ctx:i}}}),z=new S({props:{once:!0,animation:"fade-in 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000) both",$$slots:{default:[ze]},$$scope:{ctx:i}}}),T=new S({props:{once:!0,animation:"slide-bottom 1.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;",$$slots:{default:[Ce]},$$scope:{ctx:i}}}),R=new S({props:{once:!0,animation:"fade-in 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000) both",$$slots:{default:[Te]},$$scope:{ctx:i}}}),W=new S({props:{once:!0,animation:"slide-left 1.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;",$$slots:{default:[Ve]},$$scope:{ctx:i}}}),{c(){e=c("meta"),a=v(),t=c("div"),h&&h.c(),o=v(),f=c("div"),A=c("div"),x(D.$$.fragment),ae=v(),U=c("div"),O=c("div"),x(N.$$.fragment),oe=v(),x(G.$$.fragment),ne=v(),k=c("div"),x(L.$$.fragment),re=v(),E=c("div"),x(H.$$.fragment),le=v(),F=c("div"),x(z.$$.fragment),ie=v(),C=c("div"),x(T.$$.fragment),ce=v(),V=c("div"),J=c("div"),x(R.$$.fragment),ue=v(),x(W.$$.fragment),this.h()},l(s){const r=$e("svelte-lt29t9",document.head);e=u(r,"META",{name:!0,content:!0,class:!0}),r.forEach(l),a=$(s),t=u(s,"DIV",{class:!0,style:!0});var Q=p(t);h&&h.l(Q),Q.forEach(l),o=$(s),f=u(s,"DIV",{class:!0});var _=p(f);A=u(_,"DIV",{class:!0});var j=p(A);w(D.$$.fragment,j),ae=$(j),U=u(j,"DIV",{class:!0});var P=p(U);O=u(P,"DIV",{class:!0});var X=p(O);w(N.$$.fragment,X),X.forEach(l),oe=$(P),w(G.$$.fragment,P),P.forEach(l),j.forEach(l),ne=$(_),k=u(_,"DIV",{class:!0});var B=p(k);w(L.$$.fragment,B),re=$(B),E=u(B,"DIV",{class:!0});var q=p(E);w(H.$$.fragment,q),le=$(q),F=u(q,"DIV",{class:!0});var Z=p(F);w(z.$$.fragment,Z),Z.forEach(l),q.forEach(l),B.forEach(l),ie=$(_),C=u(_,"DIV",{class:!0});var Y=p(C);w(T.$$.fragment,Y),ce=$(Y),V=u(Y,"DIV",{class:!0});var te=p(V);J=u(te,"DIV",{class:!0});var fe=p(J);w(R.$$.fragment,fe),fe.forEach(l),ue=$(te),w(W.$$.fragment,te),te.forEach(l),Y.forEach(l),_.forEach(l),this.h()},h(){document.title="Home",n(e,"name","description"),n(e,"content","Brief Introduction to IASMUN's Main Organizers"),n(e,"class","svelte-xo0eug"),n(t,"class","intro svelte-xo0eug"),n(t,"style",`background-image: url(${Ae})`),n(O,"class","text svelte-xo0eug"),n(U,"class","content svelte-xo0eug"),n(A,"class","container sg svelte-xo0eug"),n(F,"class","text svelte-xo0eug"),n(E,"class","content svelte-xo0eug"),n(k,"class","container deputy-sg svelte-xo0eug"),n(J,"class","text svelte-xo0eug"),n(V,"class","content svelte-xo0eug"),n(C,"class","container chief-advisor svelte-xo0eug"),n(f,"class","letters svelte-xo0eug")},m(s,r){d(document.head,e),m(s,a,r),m(s,t,r),h&&h.m(t,null),m(s,o,r),m(s,f,r),d(f,A),y(D,A,null),d(A,ae),d(A,U),d(U,O),y(N,O,null),d(U,oe),y(G,U,null),d(f,ne),d(f,k),y(L,k,null),d(k,re),d(k,E),y(H,E,null),d(E,le),d(E,F),y(z,F,null),d(f,ie),d(f,C),y(T,C,null),d(C,ce),d(C,V),d(V,J),y(R,J,null),d(V,ue),y(W,V,null),ee=!0,he||(de=be(pe,"resize",i[2]),he=!0)},p(s,[r]){s[0]?h?r&1&&g(h,1):(h=me(),h.c(),g(h,1),h.m(t,null)):h&&(h.d(1),h=null);const Q={};r&8&&(Q.$$scope={dirty:r,ctx:s}),D.$set(Q);const _={};r&8&&(_.$$scope={dirty:r,ctx:s}),N.$set(_);const j={};r&8&&(j.$$scope={dirty:r,ctx:s}),G.$set(j);const P={};r&8&&(P.$$scope={dirty:r,ctx:s}),L.$set(P);const X={};r&8&&(X.$$scope={dirty:r,ctx:s}),H.$set(X);const B={};r&8&&(B.$$scope={dirty:r,ctx:s}),z.$set(B);const q={};r&8&&(q.$$scope={dirty:r,ctx:s}),T.$set(q);const Z={};r&8&&(Z.$$scope={dirty:r,ctx:s}),R.$set(Z);const Y={};r&8&&(Y.$$scope={dirty:r,ctx:s}),W.$set(Y)},i(s){ee||(g(h),g(D.$$.fragment,s),g(N.$$.fragment,s),g(G.$$.fragment,s),g(L.$$.fragment,s),g(H.$$.fragment,s),g(z.$$.fragment,s),g(T.$$.fragment,s),g(R.$$.fragment,s),g(W.$$.fragment,s),ee=!0)},o(s){I(D.$$.fragment,s),I(N.$$.fragment,s),I(G.$$.fragment,s),I(L.$$.fragment,s),I(H.$$.fragment,s),I(z.$$.fragment,s),I(T.$$.fragment,s),I(R.$$.fragment,s),I(W.$$.fragment,s),ee=!1},d(s){s&&(l(a),l(t),l(o),l(f)),l(e),h&&h.d(),M(D),M(N),M(G),M(L),M(H),M(z),M(T),M(R),M(W),he=!1,de()}}}function We(i,e,a){let t=!1;_e(()=>{a(0,t=!0),a(1,o=window.innerWidth)});let o;function f(){a(1,o=pe.innerWidth)}return[t,o,f]}class Fe extends xe{constructor(e){super(),we(this,e,We,Re,ve,{})}}export{Fe as component};
