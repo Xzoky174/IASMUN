e56f9ff61db5121a15ad2f811b9d49c16cb5400b
Xzoky174 <xzoky123@gmail.com>
2023-11-21 16:29:51 +0400

    Updates

--- _app/immutable/chunks/stores.A9aXmatI.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./singletons.Gps1LlpG.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
